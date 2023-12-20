3025fe53f511a82445114171a20295d1c86e4cd8
IvanL <antila3567@gmail.com>
2023-12-20 11:33:13 +0200

    Initial Commit

---
+++ SocialMediaApp/SocialMediaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAppApp.swift
+//  SocialMediaApp
+//
+//  Created by Иван Легенький on 20.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
